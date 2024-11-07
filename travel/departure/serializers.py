3bd46145c31d49a8092c8f7932a33cabdac3fd19
Suman Bhatta <119277749+suman154@users.noreply.github.com>
2024-11-07 12:27:50 +0545

    Delete travel directory

--- travel/departure/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Departure
-
-class DepartureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Departure
-        fields = '__all__'
--- travel/departure/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    # path('create/', DepartureCreate.as_view, name='departure-create'),    
-]
